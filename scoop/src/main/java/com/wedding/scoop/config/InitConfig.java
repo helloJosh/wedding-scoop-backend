019ebcac11849c752bb8f7291de7040b87f90275
helloJosh <kimjoshua135@gmail.com>
2025-04-16 20:54:45 +0900

    fix: oracle config path 변경

--- scoop/src/main/java/com/wedding/scoop/config/InitConfig.java
+++ scoop/src/main/java/com/wedding/scoop/config/InitConfig.java
@@ -1,36 +1,36 @@
-package com.wedding.scoop.config;
-
-import com.wedding.scoop.support.DataUploader;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-@Component
-@RequiredArgsConstructor
-@Slf4j
-public class InitConfig implements CommandLineRunner {
-    private final DataUploader dataUploader;
-
-    @Value("${spring.jpa.properties.hibernate.hbm2ddl.auto:}")
-    private String ddlAuto;
-
-    @Override
-    public void run(String... args) throws Exception {
-        if ("create".equalsIgnoreCase(ddlAuto)) {
-            log.info("DDL is set to 'create' → Running data upload.");
-            dataUploader.saveVendor();
-
-            dataUploader.saveVendorCategory();
-
-            dataUploader.saveVendorOption();
-
-            dataUploader.saveVendorType();
-
-            dataUploader.upload();
-        } else {
-            log.info("DDL is '{}', skipping data upload.", ddlAuto);
-        }
-    }
-}
+//package com.wedding.scoop.config;
+//
+//import com.wedding.scoop.support.DataUploader;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@RequiredArgsConstructor
+//@Slf4j
+//public class InitConfig implements CommandLineRunner {
+//    private final DataUploader dataUploader;
+//
+//    @Value("${spring.jpa.properties.hibernate.hbm2ddl.auto:}")
+//    private String ddlAuto;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        if ("create".equalsIgnoreCase(ddlAuto)) {
+//            log.info("DDL is set to 'create' → Running data upload.");
+//            dataUploader.saveVendor();
+//
+//            dataUploader.saveVendorCategory();
+//
+//            dataUploader.saveVendorOption();
+//
+//            dataUploader.saveVendorType();
+//
+//            dataUploader.upload();
+//        } else {
+//            log.info("DDL is '{}', skipping data upload.", ddlAuto);
+//        }
+//    }
+//}
--- scoop/src/main/java/com/wedding/scoop/config/ObjectClientConfig.java
+++ scoop/src/main/java/com/wedding/scoop/config/ObjectClientConfig.java
@@ -8,31 +8,40 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.*;
+import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 
 @Configuration
 public class ObjectClientConfig {
     @Bean
     public ObjectStorage objectStorage() throws Exception {
-        // 클래스패스에 있는 `.oci/config` 리소스를 읽어서 임시 파일로 저장
-        InputStream configInputStream = getClass().getClassLoader().getResourceAsStream(".oci/config");
 
-        if (configInputStream == null) {
-            throw new FileNotFoundException("OCI config file not found in resources/.oci/config");
+        // 1. resources/.oci 디렉토리 경로 가져오기
+        URL ociDirUrl = getClass().getClassLoader().getResource(".oci");
+        if (ociDirUrl == null) {
+            throw new FileNotFoundException("resources/.oci directory not found");
         }
 
-        // 임시 파일 생성
-        File tempConfigFile = File.createTempFile("oci-config", ".tmp");
-        tempConfigFile.deleteOnExit();
+        // 2. 리소스 디렉토리를 임시 디렉토리로 복사
+        Path tempDir = Files.createTempDirectory("oci-config");
+        File ociDir = new File(ociDirUrl.toURI());
 
-        try (OutputStream out = new FileOutputStream(tempConfigFile)) {
-            configInputStream.transferTo(out);
+        for (File file : Objects.requireNonNull(ociDir.listFiles())) {
+            Path destPath = tempDir.resolve(file.getName());
+            try (InputStream in = new FileInputStream(file)) {
+                Files.copy(in, destPath, StandardCopyOption.REPLACE_EXISTING);
+            }
         }
 
-        // 생성된 임시 파일 경로로 ConfigFileAuthenticationDetailsProvider 초기화
+        // 3. 복사한 config 경로로 Provider 생성
+        Path configPath = tempDir.resolve("config");
         ConfigFileAuthenticationDetailsProvider provider =
-                new ConfigFileAuthenticationDetailsProvider(tempConfigFile.getAbsolutePath(), "DEFAULT");
+                new ConfigFileAuthenticationDetailsProvider(configPath.toString(), "DEFAULT");
 
+        // 4. ObjectStorageClient 생성
         return new ObjectStorageClient(provider);
     }
 }
